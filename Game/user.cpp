3d0e55e49dcd038b0f0df0da1a680088ef2ca509
RoscaSilviu <silviu.rosca@student.unitbv.ro>
2023-10-27 19:20:43 +0300

    Added setters for user.cpp (to be modified when Points becomes a module)

--- Game/user.cpp
+++ Game/user.cpp
@@ -20,5 +20,18 @@ std::string User::getPassword() {
 }
 
 Points User::getPoints() {
+
     return m_points;
+}
+
+void User::setName(const std::string& name) {
+    m_name = name;
+}
+
+void User::setPassword(const std::string& password) {
+    m_password = password;
+}
+
+void User::setPoints(const Points& points) {
+    m_points = points;
 }
