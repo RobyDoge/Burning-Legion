5afa469b7a7c837741441d638de98e9a3cabbb72
CristinaCrihana <147138097+bunnytina@users.noreply.github.com>
2023-10-28 15:23:57 +0300

    Added con.+decon. and getters for word class

--- Game/Word.cpp
+++ Game/Word.cpp
@@ -1,1 +1,27 @@
 #include "Word.h"
+
+Word::Word()
+{
+	m_word = "";
+}
+
+Word::~Word()
+{
+}
+
+Word::Word(const Word& word)
+{
+	m_word = word.m_word;
+	m_lexicalFamily = word.m_lexicalFamily;
+}
+
+std::string Word::getWord() const
+{
+	return m_word;
+}
+
+std::vector<std::string> Word::getLexicalFamily() const
+{
+	return m_lexicalFamily;
+}
+
--- Game/Word.h
+++ Game/Word.h
@@ -1,15 +1,16 @@
 #pragma once
 #include<string>
+#include<vector>
 class Word
 {
 public:
 	Word();
 	~Word();
 	Word(const Word& word);
-	std::string getWord();
-	std::string getLexicalFamily();
+	std::string getWord() const;
+	std::vector<std::string> getLexicalFamily() const;
 private:
 	std::string m_word;
-	std::string m_lexicalFamily;
+	std::vector<std::string> m_lexicalFamily;
 };
 
