e5ee6b65096f0e00859d6ebc3953b63af2926647
CristinaCrihana <147138097+bunnytina@users.noreply.github.com>
2023-11-05 20:52:30 +0200

    Added login method implementations

---
+++ Game/login.cpp
@@ -0,0 +1,31 @@
+import login;
+
+using game::Login;
+
+Login::Login(User& user) : m_user(user) {}
+
+Login::~Login() = default;
+
+bool Login::Authenticate(const std::string& password) const 
+{
+    return m_user.GetPassword() == password;
+}
+
+void Login::ChangePassword(const std::string& newPassword)
+{
+    m_user.SetPassword(newPassword);
+}
+
+std::string Login::ReadPassword() const
+{
+    std::string enteredPassword;
+    std::cout << "Enter your password: ";
+    char c;
+    while ((c = std::getchar()) != '\n') {
+        if (c != '\r') {
+            enteredPassword.push_back(c);  
+            std::cout << "*"; 
+        }
+    }
+    std::cout << std::endl;
+}
