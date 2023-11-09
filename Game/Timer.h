18ae32f2f6a68dba32b434999a5a1865e20d3e79
RoscaSilviu <silviu.rosca@student.unitbv.ro>
2023-11-09 10:34:49 +0200

    Added example of use and some documentation for timer class

--- Game/Timer.h
+++ Game/Timer.h
@@ -1,6 +1,11 @@
 #pragma once
 #include <chrono>
-
+/*
+This class has 2 main functionalities, getting the elapsed time and the delta time(when and if we will need to work with frames/second) 
+The elapsed time works by getting a new current time at every call, and substracting that from the start time (first reset). 
+It can be made slower or faster (if we want the time to go 2x faster for example ) using the timeScale variable.
+See how it can be used in main
+*/
 
 class Timer
 {
@@ -14,14 +19,14 @@ public:
 	void SetTimeScale(float timeScale);								 //Set the time scale for the timer
 	
 
-	void Reset();
+	void Reset();													 //Resets all variables to the time point when the method was called
 
 private:
 	std::chrono::high_resolution_clock::time_point m_startTime;		 // Time point when the Timer started
 	std::chrono::high_resolution_clock::time_point m_lastTick;		 // Time point of the last frame
 	std::chrono::high_resolution_clock::time_point m_currentTime;    // Current time point
 	std::chrono::duration<float> m_deltaTime;						 // Time duration between frames
-	float m_timeScale;												 // Scale to control the time
+	float m_timeScale;												 // Scale to control the speed of time
 
 };
 
