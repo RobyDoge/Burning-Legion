905254aedcc88dc8d18e11a047821b5403223559
RoscaSilviu <silviu.rosca@student.unitbv.ro>
2023-11-26 15:52:17 +0200

    Added new route for signup in server

--- Game/routing.cpp
+++ Game/routing.cpp
@@ -14,32 +14,65 @@
 //            return HandleLogin(req, userStorage);
 //        }
 //    );
-//
+//    CROW_ROUTE(app, "/signup").methods(crow::HTTPMethod::Post)(
+//        [&userDatabase](const crow::request& req) {
+//            return isUsernameAvailable(req, userStorage)
+//        }
+//    );
 //	m_app.port(69420).multithreaded().run();
 //
 //}
 //
-//crow::response Routing::HandleLogin(const crow::request& req, const UserStorage& userStorage) {
+//crow::response Routing::HandleLogin(const crow::request& req, const UserStorage& userStorage) 
+//{
 //    // Se așteaptă ca datele să fie trimise în format JSON
+//    auto json = crow::json::load(req.body);
+//
+//    if (!json) 
+//    {
+//        return crow::response(400, "Invalid JSON");
+//    }
+//
+//    // Verificare autentificare
+//    if (json["username"].s() != nullptr && json["password"].s() != nullptr)
+//    {
+//        std::string username = json["username"].s();
+//        std::string password = json["password"].s();
+//
+//        if (userStorage.Authenticate(username, password))
+//        {
+//            return crow::response(200, "Autentificare reușită");
+//        }
+//        else
+//        {
+//            return crow::response(401, "Autentificare eșuată");
+//        }
+//    }
+//    else 
+//    {
+//        return crow::response(400, "Username și password sunt necesare");
+//    }
+//}
+//
+//crow::response Routing::isUsernameAvailable(const crow::request& req, const UserStorage& userStorage) {
 //    auto json = crow::json::load(req.body);
 //
 //    if (!json) {
 //        return crow::response(400, "Invalid JSON");
 //    }
 //
-//    // Verificare autentificare
-//    if (json["username"].s() != nullptr && json["password"].s() != nullptr) {
+//    if (json["username"].s() != nullptr) {
 //        std::string username = json["username"].s();
-//        std::string password = json["password"].s();
 //
-//        if (userStorage.Authenticate(username, password)) {
-//            return crow::response(200, "Autentificare reușită");
+//        if (userDatabase.AuthenticateUsername(username)) {
+//            return crow::response(200, "Username is available");
 //        }
 //        else {
-//            return crow::response(401, "Autentificare eșuată");
+//            return crow::response(409, "Username is not available");
 //        }
 //    }
 //    else {
-//        return crow::response(400, "Username și password sunt necesare");
+//        return crow::response(400, "Username is required");
 //    }
+//
 //}
