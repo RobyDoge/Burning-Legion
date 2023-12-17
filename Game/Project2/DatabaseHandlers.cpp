74a5e3be1ea7f38dbf6afcf1f760811952347911
Stefania03 <stefania-miruna.tudor@student.unitbv.ro>
2023-12-18 00:10:44 +0200

    Modified WordDatabase select. DOES NOT WORK

--- Game/Project2/DatabaseHandlers.cpp
+++ Game/Project2/DatabaseHandlers.cpp
@@ -1,8 +1,8 @@
 #include"DatabaseHandlers.h"
 #include<fstream>
 #include<iostream>
+#include <random>
 #include<vector>
-#include<list>
 
 void PopulateDictionaryFromFile(Dictionary& dictionary, const std::string& filename)
 {
@@ -38,36 +38,79 @@ void AddNewUser(UserDatabase& users, const std::string name, const std::string& 
 
 void WordDatabaseHandle::Init()
 {
-    //time_t lastModifiedTime = 1702233024;            //will fix this magic number
-    //if (hasFileChanged("input.txt", lastModifiedTime))
+    time_t lastModifiedTime = 1702233024;            //will fix this magic number
+    if (hasFileChanged("input.txt", lastModifiedTime))
         m_db.remove_all<WordFromDictionary>();
     m_db.sync_schema();
 
     auto initalwordscount = m_db.count<WordFromDictionary>();
     if (initalwordscount == 0)
         PopulateDictionaryFromFile(m_db, "input.txt");
-
-    auto initalwordscount2 = m_db.count<WordFromDictionary>();
-    std::cout << initalwordscount2;
 }
 
-std::queue<std::string> WordDatabaseHandle::SelectWords(uint8_t wordsNeeded, uint8_t difficulty, uint8_t language)//server::Lobby::GameDifficulty difficulty
+std::queue<std::string> WordDatabaseHandle::SelectWords(uint8_t numberOfPlayers, uint8_t difficulty, uint8_t language)
 {
     std::queue<std::string> generatedWords;
-    auto rows = m_db.select(sqlite_orm::columns(&WordFromDictionary::word),
+
+    std::string languageName;
+    switch (language)
+    {
+    case 0:
+        languageName = "eng";
+        break;
+    case 1:
+        languageName = "ro";
+        break;
+    case 2:
+        languageName = "esp";
+	    default:
+            break;
+    }
+    auto rows = m_db.select(sqlite_orm::columns(&WordFromDictionary::id),
         sqlite_orm::where(
             sqlite_orm::and_(
-                sqlite_orm::c(&WordFromDictionary::difficulty) == difficulty,
-                sqlite_orm::c(&WordFromDictionary::language) == language
-            )),
-        sqlite_orm::limit(wordsNeeded));
-    auto initalwordscount2 = m_db.count<WordFromDictionary>();
+                sqlite_orm::c(&WordFromDictionary::difficulty) == difficulty + '0',
+                sqlite_orm::c(&WordFromDictionary::language) == languageName
+            )));
+        //sqlite_orm::limit(numberOfPlayers * 4));                //create variable for number of turns pls uwu Roby
 
-     for (const auto& row : rows)                // Add words from the database to the vector
+    std::vector<uint16_t> wordIds;
+
+     for (const auto& row : rows)                // Add words from the database to an queue
     {
-        generatedWords.push(std::get<0>(row));
+        wordIds.emplace_back(std::get<0>(row));
     }
-    return generatedWords;
+
+     std::random_device rd;
+     std::mt19937 gen(rd());
+
+     // Create a distribution to generate indices in the range [0, originalVector.size())
+     std::uniform_int_distribution<std::size_t> dis(0, wordIds.size() - 1);
+
+     // Create a set to store unique indices
+     std::set<std::size_t> chosenIndices;
+
+     // Generate unique random indices
+     while (chosenIndices.size() < numberOfPlayers * 4 + 2) {
+         chosenIndices.insert(dis(gen));
+     }
+
+     for(const auto index : chosenIndices)
+     {
+         auto elements = m_db.select(sqlite_orm::columns(&WordFromDictionary::word),
+             sqlite_orm::where(
+                 sqlite_orm::c(&WordFromDictionary::id) == index)
+         );
+
+
+         for (const auto& element : elements)                // Add words from the database to an queue
+         {
+             generatedWords.emplace(std::get<0>(element));
+
+         }
+     }
+
+	return generatedWords;
 }
 
 void WordDatabaseHandle::ClearDictionary()
